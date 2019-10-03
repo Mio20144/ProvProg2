063dd4405556f25b58cd9c5817373be3aeceecf5
Olle Hasselstrom <olle.choukrihasselstrom@elev.ga.ntig.se>
2019-10-03 13:16:04 +0200

    Skrivit den mesta koden för classe Book samt gjort så att användaren får söka efter en bok

---
+++ Prog2Prov Modellering/Prog2Prov Modellering/Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2Prov_Modellering
+{
+    class Book
+    {
+        public int price;
+        private string name;
+        private int rarity;
+        private string category;
+        private int actualValue;
+        private bool cursed = false;
+        Random generator = new Random();
+        public void PrintInfo()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("The value for this book is " + actualValue);
+            Console.WriteLine("The rarity for this book is " + rarity);
+            
+        }
+        public string GetCategory()
+        {
+            return category;
+        }
+        public string GetName()
+        {
+            name = Console.ReadLine();
+            return name;
+        }
+        public int Evaluate()
+        {
+            actualValue = generator.Next(10, 100);
+            rarity = generator.Next(1, 4);
+            actualValue *= rarity;
+            /*double multiplier = generator.NextDouble(0.5 , 1.5);
+            int newMultiplier = Convert.ToInt32(multiplier);
+            actualValue *= newMultiplier;*/
+            return actualValue;
+
+        }
+        public bool IsCursed()
+        {
+            int cursingPower = generator.Next(0, 11);
+            if (cursingPower >= 3)
+            {
+                cursed = false;
+                Console.WriteLine("This book is not cursed");
+
+            }
+            else
+            {
+                cursed = true;
+                Console.WriteLine("This book is cursed");
+            }
+            return cursed;
+        }
+    }
+    
+    
+    
+}
---
+++ Prog2Prov Modellering/Prog2Prov Modellering/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2Prov_Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book b1 = new Book();
+            List<string> names = new List<string>();
+            Console.WriteLine("Please enter the book you are looking for");
+            b1.GetName();
+            b1.Evaluate();
+            
+            b1.PrintInfo();
+            b1.IsCursed();
+
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
